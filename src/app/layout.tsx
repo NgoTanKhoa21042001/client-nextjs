51ba7538e69cacd53fb954d9a5fc5f46206f400c
NgoTanKhoa21042001 <kakashi21042001@gmail.com>
2024-03-07 16:28:46 +0700

    Sử dụng Fonts trong Next.js (Google Fonts, Local Fonts, Tailwind CSS)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,25 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
+// import { Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300"] });
+// Font files can be colocated inside of `app`
+const myFont = localFont({
+  src: [
+    {
+      path: "./Roboto-Thin.ttf",
+      weight: "100",
+    },
+    {
+      path: "./Roboto-Regular.ttf",
+      weight: "400",
+    },
+  ],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${myFont.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,7 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
     <>
-      <div className="p-5">
-        <Header />
-        <Button variant={"destructive"} className="p-10 m-4 text-mini">
-          Mua Khóa học NextJS
-        </Button>
-        <Input type="email" placeholder="email" />
-        <Card />
-      </div>
+      <h1 className="font-sans text-4xl text-center">Xin chaof moi ngueoi</h1>
     </>
   );
 }
